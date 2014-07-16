e3386155eb82318fbc04591081afb844db0fe4bc
Paul Eipper <paul@nkey.com.br>
2014-07-16 15:21:25 -0300

    Rewrite to fix init Model with dictionary

--- schemalchemy.py
+++ schemalchemy.py
@@ -74,34 +74,55 @@ class SchemAlchemyModelMeta(schematics.models.ModelMeta, DeclarativeMeta):
             mapper.add_property(field_name, field_synonym)
 
 
-def _reconstructor(self):
+class SchemAlchemyModel(schematics.models.Model):
     """
-    Call Schematics __init__ and trigger the descriptors for all mapped fields
-    when loading from database.
+    Set columns on init and trigger the descriptors for all mapped fields when
+    loading from database.
 
     """
-    self.__init__()
-    cls = self.__class__
-    for field_name in self._fields:
-        field_descriptor = cls.__dict__.get(field_name)
-        if not hasattr(field_descriptor, 'column_name'):
-            continue
-        column_name = field_descriptor.column_name
-        value = orm.base.instance_dict(self).get(column_name)
-        setattr(self, field_name, value)
+
+    __mapper_args__ = {'column_prefix': '_'}
+
+    def __init__(self, *a, **kw):
+        super(SchemAlchemyModel, self).__init__(*a, **kw)
+        self._set_mapped_column_values()
+
+    @orm.reconstructor
+    def _reconstructor(self):
+        super(SchemAlchemyModel, self).__init__()
+        self._set_mapped_field_values()
+
+    def _iter_column_fields(self):
+        cls = self.__class__
+        for field_name in self._fields:
+            field_descriptor = cls.__dict__.get(field_name)
+            if not hasattr(field_descriptor, 'column_name'):
+                continue
+            column_name = field_descriptor.column_name
+            yield (field_name, column_name)
+
+    def _set_mapped_field_values(self):
+        for field_name, column_name in self._iter_column_fields():
+            value = orm.base.instance_dict(self).get(column_name)
+            setattr(self, field_name, value)
+
+    def _set_mapped_column_values(self):
+        for field_name, column_name in self._iter_column_fields():
+            instance_dict = orm.base.instance_dict(self)
+            instance_dict[column_name] = getattr(self, field_name)
 
 
 # Schematics monkeypatching
 
 schematics.models.FieldDescriptor = SchemAlchemyFieldDescriptor
 schematics.models.ModelMeta = SchemAlchemyModelMeta
-schematics.models.Model.__mapper_args__ = {'column_prefix': '_'}
-schematics.models.Model._reconstructor = orm.reconstructor(_reconstructor)
+schematics.models.Model = SchemAlchemyModel
 
 
 # For model definition inherit from the `Base` class below instead of `Model`
 
 Base = declarative_base(
-    cls=schematics.models.Model,
+    cls=SchemAlchemyModel,
     metaclass=SchemAlchemyModelMeta,
     constructor=None)
+
